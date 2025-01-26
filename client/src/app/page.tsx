da6a09ae24b95d0ac4cfee37e643d9c5dadccfce
Babette Bækgaard (BabetteB) <babetteb@outlook.dk>
2025-01-26 15:07:45 +0100

    removed template code, updated front page and added test data (just temp to visualize when there is content)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
+import Collection from "./components/collections/collection";
+import NewCollection from "./components/collections/new-collection";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className={styles.main}>
+      <div className={styles.title_pane}>
+        <h2>
+          Flash Card Generator
+        </h2>
+      </div>
+      <div className={styles.collection_pane}>
+        <h3>My Collections</h3>
+        <div className="divider"></div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <NewCollection/>
+
+        <div className="divider"></div>
+
+        <div className={styles.collections_container}>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
+          <Collection collectionName="Test collection very vereeeeeeeeeeeeeeeeeeeeeey very very very very very very very very very very very very very very very very very very veeeeeeeeeeeery very very very very very very very very very long name"/>
+          <Collection collectionName="Test collection name 2"/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
